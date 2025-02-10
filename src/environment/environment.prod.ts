bced17badebea83b005617dd0e21f02178871d8e
Roushan Prakash <roushanprakash123@gmail.com>
2025-02-10 13:44:51 +0530

    Update environment.prod.ts

--- src/environment/environment.prod.ts
+++ src/environment/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8080/api/api',
+  apiUrl: 'http://banking-portal-api-v3-banking-admin-crt-dev.apps.rm3.7wse.p1.openshiftapps.com/api',
   tokenName :  "authToken",
   origin:'http://localhost:4200' 
 };
